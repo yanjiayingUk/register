8e3f8256482f3b053db62ab038efae5eab6a75a0
“yanjiayingUk <1999jiulanyouji>
2020-03-17 17:09:24 +0800

    travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,29 @@
 /* global module: true */
 module.exports = function (grunt) {
     grunt.initConfig({
+        htmlmin:{
+            options:{
+                removeComments:true,
+                collapseWhitespace:true
+            },
+            files: {
+                src:'./index.html',
+                dest:'dist/index.html'
+            }
+        },
         cssmin: {
-            'register.min.css':'register.css'
+            'dist/register.css':'register.css'
+        },
+        uglify: {
+            files: {'dist/rejister.js':'rejster.js'}
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
+    grunt.registerTask('default', ['htmlmin']);
     grunt.registerTask('default', ['cssmin'])
+    grunt.registerTask('default', ['uglify'])
 }
